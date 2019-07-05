29e4f3c0d736c8df53b40dc698cf61bc72e9802f
Ross Mawdsley <rossmawd@gmail.com>
2019-07-05 15:49:06 +0100

    contribution controller and seeds-to be merged/replaced

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,15 @@
 class ContributionsController < ApplicationController
+  def show
+    contribution = Contribution.find_by(id: params[:id])
+    if contribution
+      render json: contribution
+    else
+      render json: { error: "Contribution not found." }, status: 404
+    end
+  end
+
+  def index
+    contributions = Contribution.all
+    render json: contributions
+  end
 end
