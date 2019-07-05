7c08aa3691aff90c0fb3f2ef8eccd8a9b5052e07
Ross Mawdsley <rossmawd@gmail.com>
2019-07-05 10:20:02 +0100

    added name to event (migration)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_162534) do
+ActiveRecord::Schema.define(version: 2019_07_05_091903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_07_04_162534) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -43,10 +44,10 @@ ActiveRecord::Schema.define(version: 2019_07_04_162534) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "password_digest"
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
   end
 
   add_foreign_key "contributions", "events"
