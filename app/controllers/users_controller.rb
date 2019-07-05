b0727b7bf1591dbf929b743a31ccccca5ef4d9df
Ayleen Nazario <ayleen.nazario@gmail.com>
2019-07-05 18:49:18 +0100

    Added User show and signin methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+def show 
+    user = User.find_by(id: params[:id])
+    if user
+        render json:user
+    else 
+        render json: {error: 'User not found.'}, status: 404
+    end 
+end 
+
+def signin
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+        render json: user 
+    else 
+        render json: { error: 'Invalid username/password combination.' }, status: 401
+    end
+end
+
+# def new
+#     user = User.new
+#   end
+
+#   def create
+#     user = User.create user_params
+#     if user.valid?
+#       user.save
+#       session[:user_id] = user.id
+#       redirect_to user_path(user)
+#     else
+#       flash[:errors] = user.errors.full_messages
+#       redirect_to new_user_path
+#     end
+#   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :likes
   resources :contributions
   resources :events
-  resources :users
+  resources :users, only: [:show]
+
+
+  post '/signin', to: 'users#signin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
